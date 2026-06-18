11d987011389fb139e728091c377b2b55524eb63
Luna Long <eggsim49@gmail.com>
2026-06-18 23:03:51 +0100

    rustlings

--- rustlings/exercises/11_hashmaps/hashmaps2.rs
+++ rustlings/exercises/11_hashmaps/hashmaps2.rs
@@ -33,7 +33,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
 
-        basket.insert(String::from("banana"), 2);
+        basket.entry(fruit).or_insert(1);
     }
 }
 
